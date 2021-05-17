2f8d65bd41cb6ccb440b5acc553dd98b4bea1ad1
mccbc <cmcclellan1010@gmail.com>
2021-05-17 15:49:15 -0400

    adding docs to parameters

--- eigencode/efunctions.py
+++ eigencode/efunctions.py
@@ -1,9 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from constants import fundconst,lymanalpha
+from constants import fundconst, lymanalpha
 from scipy.integrate import solve_ivp, odeint
-from parameters import Parameters, get_sigma_bounds
-#from rk import rk
+from parameters import Parameters
+from util import line_profile, get_sigma_bounds
 import warnings
 import pdb
 
@@ -16,143 +16,148 @@ import pdb
 # [ ] Insert zero at line center for Jsoln?
 # [ ] rtol and atol convergence
 
-fc=fundconst()
-la=lymanalpha()
+fc = fundconst()
+la = lymanalpha()
 
-def line_profile(sigma,p):					# units of Hz^{-1}
-  x=(np.abs(sigma)/p.c1)**(1.0/3.0)
-  #line_profile = ( np.exp(-x**2)/np.sqrt(np.pi) + p.a/np.pi/(0.01+x**2) ) / p.Delta		# doppler and natural
-  line_profile = p.a / np.pi / (x**2) / p.Delta
-  return line_profile
+
+
 
 # Differential equation for solve_ivp
-def func(sigma,y,n,s,p):
-  J = y[0]
-  dJ = y[1]
-  phi=line_profile(sigma,p)
-  kappan=n*np.pi/p.radius
-  wavenum = kappan * p.Delta / p.k
-  term1 = wavenum**2 
-  term2 = 3.0*s*p.Delta**2*phi/(fc.clight*p.k)
-  dydsigma=np.zeros(3)
-  dydsigma[0]=dJ
-  dydsigma[1]= (term1+term2) * J
-  dydsigma[2]=J
-  return dydsigma
+
+
+def func(sigma, y, n, s, p):
+    J = y[0]
+    dJ = y[1]
+    phi = line_profile(sigma, p)
+    kappan = n * np.pi / p.radius
+    wavenum = kappan * p.Delta / p.k
+    term1 = wavenum**2
+    term2 = 3.0 * s * p.Delta**2 * phi / (fc.clight * p.k)
+    dydsigma = np.zeros(3)
+    dydsigma[0] = dJ
+    dydsigma[1] = (term1 + term2) * J
+    dydsigma[2] = J
+    return dydsigma
 
 
 def integrate(sigma_bounds, y_start, n, s, p):
-  '''
-  Returns interpolants which can be used to evaluate the function at any sigma
-  '''
-  sol = solve_ivp(func, [sigma_bounds[0], sigma_bounds[1]], y_start, args=(n,s,p), rtol=1e-10, atol=1e-10, dense_output=True)
-  return sol.y.T, sol.sol
+    '''
+    Returns interpolants which can be used to evaluate the function at any sigma
+    '''
+    sol = solve_ivp(func, [sigma_bounds[0], sigma_bounds[1]], y_start, args=(
+        n, s, p), rtol=1e-10, atol=1e-10, dense_output=True)
+    return sol.y.T, sol.sol
 
 
 def one_s_value(n, s, p):
-  '''Solve for response given n and s'''
+    '''Solve for response given n and s'''
 
-  kappan = n*np.pi/p.radius
-  wavenum = kappan * p.Delta / p.k
+    kappan = n * np.pi / p.radius
+    wavenum = kappan * p.Delta / p.k
 
-  # Construct grids for this value of n and s
-  left, middle, right = get_sigma_bounds(n, s, p)
+    # Construct grids for this value of n and s
+    left, middle, right = get_sigma_bounds(n, s, p)
 
-  # rightward integration
-  J = 1.0
-  dJ = wavenum*J
-  y_start = np.array( (J, dJ, 0.0) )
-  sol, interp_left = integrate(left, y_start, n, s, p)
-  Jleft = sol[:,0]
-  dJleft = sol[:,1]
-  intJdsigmaleft = sol[:,2]
-  A = Jleft[-1]    # Set matrix coefficient equal to Jleft's rightmost value
-  B = dJleft[-1]
-  left_P = intJdsigmaleft[-1]
+    # rightward integration
+    J = 1.0
+    dJ = wavenum * J
+    y_start = np.array((J, dJ, 0.0))
+    sol, interp_left = integrate(left, y_start, n, s, p)
+    Jleft = sol[:, 0]
+    dJleft = sol[:, 1]
+    intJdsigmaleft = sol[:, 2]
+    A = Jleft[-1]    # Set matrix coefficient equal to Jleft's rightmost value
+    B = dJleft[-1]
+    left_P = intJdsigmaleft[-1]
 
-  # leftward integration
-  J = 1.0
-  dJ = -wavenum*J
-  y_start = np.array( (J, dJ, 0.0) )
-  sol, interp_right = integrate(right[::-1], y_start, n, s, p)
-  Jright = sol[:,0]
-  dJright = sol[:,1]
-  intJdsigmaright = sol[:,2]
-  C = Jright[-1]   # Set matrix coefficient equal to Jright's leftmost value
-  D = dJright[-1]
-  right_P = intJdsigmaright[-1]
+    # leftward integration
+    J = 1.0
+    dJ = -wavenum * J
+    y_start = np.array((J, dJ, 0.0))
+    sol, interp_right = integrate(right[::-1], y_start, n, s, p)
+    Jright = sol[:, 0]
+    dJright = sol[:, 1]
+    intJdsigmaright = sol[:, 2]
+    C = Jright[-1]   # Set matrix coefficient equal to Jright's leftmost value
+    D = dJright[-1]
+    right_P = intJdsigmaright[-1]
 
-  # middle integration
-  # If source > 0, integrate rightward from 0 to source, matching at 0
-  # Middlegrid is ordered increasingly, starting at 0 and going to source
-  if p.sigmas > 0.:
+    # middle integration
+    # If source > 0, integrate rightward from 0 to source, matching at 0
+    # Middlegrid is ordered increasingly, starting at 0 and going to source
+    if p.sigmas > 0.:
 
-      # Match initial conditions at 0 (rightward integration)
-      J = Jleft[-1]
-      dJ = dJleft[-1]
-      y_start = np.array((J, dJ, left_P))
+        # Match initial conditions at 0 (rightward integration)
+        J = Jleft[-1]
+        dJ = dJleft[-1]
+        y_start = np.array((J, dJ, left_P))
 
-      # Find solution in middle region
-      sol, interp_middle = integrate(middle, y_start, n, s, p)
-      Jmiddle = sol[:,0]
-      dJmiddle = sol[:,1]
-      intJdsigmamiddle = sol[:,2]
+        # Find solution in middle region
+        sol, interp_middle = integrate(middle, y_start, n, s, p)
+        Jmiddle = sol[:, 0]
+        dJmiddle = sol[:, 1]
+        intJdsigmamiddle = sol[:, 2]
 
-      # Set coefficients of matrix equation at the source
-      A = Jmiddle[-1]    # Overwrite previous matrix coefficients
-      B = dJmiddle[-1]
-      left_P = intJdsigmamiddle[-1]
+        # Set coefficients of matrix equation at the source
+        A = Jmiddle[-1]    # Overwrite previous matrix coefficients
+        B = dJmiddle[-1]
+        left_P = intJdsigmamiddle[-1]
 
-      scale_right = - 1.0/(D-B*(C/A)) * np.sqrt(6.0)/8.0 * n**2 * p.energy/(p.k*p.radius**3)
-      scale_left = C/A * scale_right
-      scale_middle = scale_left
+        scale_right = - 1.0 / (D - B * (C / A)) * np.sqrt(6.0) / \
+            8.0 * n**2 * p.energy / (p.k * p.radius**3)
+        scale_left = C / A * scale_right
+        scale_middle = scale_left
 
-      scales = [scale_left, scale_middle, scale_right]
-      interps = [interp_left, interp_middle, interp_right]
+        scales = [scale_left, scale_middle, scale_right]
+        interps = [interp_left, interp_middle, interp_right]
 
-  # If source < 0, integrate leftward from 0 to source, matching at 0
-  # Middlegrid is ordered decreasingly, starting at zero and going to source
-  elif p.sigmas < 0.:
+    # If source < 0, integrate leftward from 0 to source, matching at 0
+    # Middlegrid is ordered decreasingly, starting at zero and going to source
+    elif p.sigmas < 0.:
 
-      # Match initial conditions at 0 (leftward integration)
-      J = Jright[-1]
-      dJ = dJright[-1]
-      y_start = np.array((J, dJ, right_P))
+        # Match initial conditions at 0 (leftward integration)
+        J = Jright[-1]
+        dJ = dJright[-1]
+        y_start = np.array((J, dJ, right_P))
 
-      # Find solution in middle region
-      sol, interp_middle = integrate(middle[::-1], y_start, n, s, p)
-      Jmiddle=sol[:,0]
-      dJmiddle=sol[:,1]
-      intJdsigmamiddle = sol[:,2]
+        # Find solution in middle region
+        sol, interp_middle = integrate(middle[::-1], y_start, n, s, p)
+        Jmiddle = sol[:, 0]
+        dJmiddle = sol[:, 1]
+        intJdsigmamiddle = sol[:, 2]
 
-      # Set coefficients of matrix equation at the source
-      C = Jmiddle[-1]   # Overwrite previous matrix coefficients
-      D = dJmiddle[-1]
-      right_P = intJdsigmamiddle[-1]
+        # Set coefficients of matrix equation at the source
+        C = Jmiddle[-1]   # Overwrite previous matrix coefficients
+        D = dJmiddle[-1]
+        right_P = intJdsigmamiddle[-1]
 
-      scale_right = - 1.0/(D-B*(C/A)) * np.sqrt(6.0)/8.0 * n**2 * p.energy/(p.k*p.radius**3)
-      scale_left = C/A * scale_right
-      scale_middle = scale_right
+        scale_right = - 1.0 / (D - B * (C / A)) * np.sqrt(6.0) / \
+            8.0 * n**2 * p.energy / (p.k * p.radius**3)
+        scale_left = C / A * scale_right
+        scale_middle = scale_right
 
-      scales = [scale_left, scale_middle, scale_right]
-      interps = [interp_left, interp_middle, interp_right]
+        scales = [scale_left, scale_middle, scale_right]
+        interps = [interp_left, interp_middle, interp_right]
 
-  # If source = 0, do nothing
-  else:
-      scale_right = - 1.0/(D-B*(C/A)) * np.sqrt(6.0)/8.0 * n**2 * p.energy/(p.k*p.radius**3)
-      scale_left = C/A * scale_right
+    # If source = 0, do nothing
+    else:
+        scale_right = - 1.0 / (D - B * (C / A)) * np.sqrt(6.0) / \
+            8.0 * n**2 * p.energy / (p.k * p.radius**3)
+        scale_left = C / A * scale_right
 
-      scales = [scale_left, scale_right]
-      interps = [interp_left, interp_right]
+        scales = [scale_left, scale_right]
+        interps = [interp_left, interp_right]
 
-  J, dJ = np.zeros(p.nsigma), np.zeros(p.nsigma)
-  for i in range(len(scales)):
-      mask = np.logical_and(p.sigma <= interps[i].t_max, p.sigma >= interps[i].t_min)
-      inds = np.where(mask)
-      J[inds], dJ[inds], _ = interps[i](p.sigma[mask]) * scales[i]
-  intJdsigma = left_P*scale_left - right_P*scale_right
+    J, dJ = np.zeros(p.nsigma), np.zeros(p.nsigma)
+    for i in range(len(scales)):
+        mask = np.logical_and(
+            p.sigma <= interps[i].t_max,
+            p.sigma >= interps[i].t_min)
+        inds = np.where(mask)
+        J[inds], dJ[inds], _ = interps[i](p.sigma[mask]) * scales[i]
+    intJdsigma = left_P * scale_left - right_P * scale_right
 
-  return J, dJ, intJdsigma
+    return J, dJ, intJdsigma
 
 
 def solve(s1, s2, s3, n, p):
@@ -161,95 +166,109 @@ def solve(s1, s2, s3, n, p):
     given three frequencies s1, s2, s3.
     '''
 
-    if s1>s3:
+    if s1 > s3:
         s1, s3 = s3, s1
 
     J1, dJ1, n1 = one_s_value(n, s1, p)
     J2, dJ2, n2 = one_s_value(n, s2, p)
     J3, dJ3, n3 = one_s_value(n, s3, p)
 
-    err=1.e20
-    while err>1.e-3:
-        ratio1 = (n2-n1) / (n2-n3)
-        ratio2 = ratio1 * (s3-s2) / (s1-s2)
-        sguess = (s1*ratio2 - s3)/(ratio2 - 1.0)
+    err = 1.e20
+    while err > 1.e-3:
+        ratio1 = (n2 - n1) / (n2 - n3)
+        ratio2 = ratio1 * (s3 - s2) / (s1 - s2)
+        sguess = (s1 * ratio2 - s3) / (ratio2 - 1.0)
         Jguess, dJguess, nguess = one_s_value(n, sguess, p)
 
         print("\nguess:")
-        print("s:    {:.6f}    {:.6f}    {:.6f}".format(sguess/s1, sguess/s2, sguess/s3))
-        print("n:    {:.1e}    {:.1e}    {:.1e}".format(nguess/n1, nguess/n2, nguess/n3))
+        print("s:    {:.6f}    {:.6f}    {:.6f}".format(
+            sguess / s1, sguess / s2, sguess / s3))
+        print("n:    {:.1e}    {:.1e}    {:.1e}".format(
+            nguess / n1, nguess / n2, nguess / n3))
 
-        if (sguess - s1)*(sguess - s2) < 0.0:
+        if (sguess - s1) * (sguess - s2) < 0.0:
             s3, J3, n3 = s2, J2, n2
         else:
             s1, J1, n1 = s2, J2, n2
         s2, J2, n2 = sguess, Jguess, nguess
-        err=np.abs((s3-s1)/s2)
+        err = np.abs((s3 - s1) / s2)
 
-    sres=s2
-    Jres = (J3-J1)*(s3-sres)*(s1-sres)/(s1-s3)
-    nres = (n3-n1)*(s3-sres)*(s1-sres)/(s1-s3)
+    sres = s2
+    Jres = (J3 - J1) * (s3 - sres) * (s1 - sres) / (s1 - s3)
+    nres = (n3 - n1) * (s3 - sres) * (s1 - sres) / (s1 - s3)
     return sres, Jres, nres
 
+
 def sweep(p, nmin=1, output_dir=None):
-  # loop over n and s=-i\omega. when you find a maximum in the size of the response, call the solve function
-  # tabulate s(n,m) and J(n,m,sigma).
+    # loop over n and s=-i\omega. when you find a maximum in the size of the response, call the solve function
+    # tabulate s(n,m) and J(n,m,sigma).
 
-  gamma_const = fc.clight/p.radius/(p.a*p.tau0)**0.333 * np.pi**(13.0/6.0)/2.0**0.333
+    gamma_const = fc.clight / p.radius / \
+        (p.a * p.tau0)**0.333 * np.pi**(13.0 / 6.0) / 2.0**0.333
 
-  for n in range(nmin,p.nmax+1):
-    print ("n=",n)
-    nsoln=1
-    s = -0.000001
-    if nmin == 1:
-        s_increment = -0.01
-    else:
-        s_increment = -0.25*gamma_const*n**(4.0/3.0)*0.667*(1+1.0/8.0)**(-1.0/3.0)
+    for n in range(nmin, p.nmax + 1):
+        print("n=", n)
+        nsoln = 1
+        s = -0.000001
+        if nmin == 1:
+            s_increment = -0.01
+        else:
+            s_increment = -0.25 * gamma_const * \
+                n**(4.0 / 3.0) * 0.667 * (1 + 1.0 / 8.0)**(-1.0 / 3.0)
 
-    norm=[]
-    while nsoln < p.mmax+1:
+        norm = []
+        while nsoln < p.mmax + 1:
 
-      J,dJ,intJdsigma=one_s_value(n,s,p)
-      norm.append(np.abs(intJdsigma))
-      print("nsoln,n,s,response=",nsoln,n,s,norm[-1])
-      if len(norm)>2 and norm[-3]<norm[-2] and norm[-1]<norm[-2]:
-        sres,Jres,intJdsigmares = solve(s-2*s_increment,s-s_increment,s,n,p)
-        out = {"s":sres, "J":Jres, "Jint":intJdsigmares}
-        np.save(output_dir/'n{:03d}_m{:03d}.npy'.format(n, nsoln), out)
+            J, dJ, intJdsigma = one_s_value(n, s, p)
+            norm.append(np.abs(intJdsigma))
+            print("nsoln,n,s,response=", nsoln, n, s, norm[-1])
+            if len(norm) > 2 and norm[-3] < norm[-2] and norm[-1] < norm[-2]:
+                sres, Jres, intJdsigmares = solve(
+                    s - 2 * s_increment, s - s_increment, s, n, p)
+                out = {"s": sres, "J": Jres, "Jint": intJdsigmares}
+                np.save(
+                    output_dir /
+                    'n{:03d}_m{:03d}.npy'.format(
+                        n,
+                        nsoln),
+                    out)
 
-        nsoln=nsoln+1
-        s_increment = -0.25*gamma_const*n**(4.0/3.0)*0.667*(nsoln+1.0/8.0)**(-1.0/3.0)
-        print("\nds={}".format(s_increment))
-      s += s_increment
-  return
+                nsoln = nsoln + 1
+                s_increment = -0.25 * gamma_const * \
+                    n**(4.0 / 3.0) * 0.667 * (nsoln + 1.0 / 8.0)**(-1.0 / 3.0)
+                print("\nds={}".format(s_increment))
+            s += s_increment
+    return
+
 
 def check_s_eq_0(p):
-    n=1
-    s=-0.00001
-    kappan=n*np.pi/p.radius
-    wavenum=kappan*p.Delta/p.k
-    J,dJ,intJdsigma=one_s_value(n,s,p)
+    n = 1
+    s = -0.00001
+    kappan = n * np.pi / p.radius
+    wavenum = kappan * p.Delta / p.k
+    J, dJ, intJdsigma = one_s_value(n, s, p)
     plt.figure()
-    plt.plot(p.sigma,J,'b-')
-    analytic = np.sqrt(6.0/np.pi)/16.0 * p.tau0 * n * p.energy/(p.k*p.radius**3) * np.exp(-wavenum*np.abs(p.sigma))
-    plt.plot(p.sigma,analytic,'r--')
+    plt.plot(p.sigma, J, 'b-')
+    analytic = np.sqrt(6.0 / np.pi) / 16.0 * p.tau0 * n * p.energy / \
+        (p.k * p.radius**3) * np.exp(-wavenum * np.abs(p.sigma))
+    plt.plot(p.sigma, analytic, 'r--')
     plt.yscale('log')
     plt.show()
     plt.close()
 
 
 if __name__ == "__main__":
-    energy=1.e0
-    temp=1.e4
-    tau0=1.e7
-    radius=1.e11
-    alpha_abs=0.0
-    prob_dest=0.0
-    xsource=0.0
-    nmin=71
-    nmax=80
-    mmax=100
-    nsigma=1024
+    energy = 1.e0
+    temp = 1.e4
+    tau0 = 1.e7
+    radius = 1.e11
+    alpha_abs = 0.0
+    prob_dest = 0.0
+    xsource = 0.0
+    nmin = 71
+    nmax = 80
+    mmax = 100
+    nsigma = 1024
 
     from pathlib import Path
     from datetime import datetime
@@ -258,14 +277,20 @@ if __name__ == "__main__":
     datestr = datetime.today().strftime('%y%m%d-%H%M')
     output_dir = Path("./data/{}".format(datestr)).resolve()
     output_dir.mkdir(parents=True, exist_ok=True)
-
     start = time.time()
-
-    p = Parameters(temp,tau0,radius,energy,xsource,alpha_abs,prob_dest,nsigma,nmax,mmax)
-    pickle.dump(p, open(output_dir/'parameters.p', 'wb'))
-
+    p = Parameters(
+        temp,
+        tau0,
+        radius,
+        energy,
+        xsource,
+        alpha_abs,
+        prob_dest,
+        nsigma,
+        nmax,
+        mmax)
+    pickle.dump(p, open(output_dir / 'parameters.p', 'wb'))
     sweep(p, nmin=nmin, output_dir=output_dir)
-
     stop = time.time()
-    with open(output_dir/'time.txt', 'w') as f:
+    with open(output_dir / 'time.txt', 'w') as f:
         f.write(str(stop - start))
--- eigencode/parameters.py
+++ eigencode/parameters.py
@@ -1,117 +1,73 @@
-from constants import fundconst,lymanalpha
-import warnings
+from constants import fundconst, lymanalpha
 import numpy as np
-import pdb
 
-fc=fundconst()
-la=lymanalpha()
+fc = fundconst()
+la = lymanalpha()
+
 
 class Parameters:
-  def __init__(self,temp,tau0,radius,energy,xsource,alpha_abs,prob_dest,nsigma,nmax,mmax):
-    self.temp=temp                                              # temperature in K
-    self.tau0=tau0                                              # line center optical depth of sphere
-    self.radius=radius                                          # radius of sphere in cm
-    self.energy=energy						# impulse energy in erg
-    self.xsource = xsource
-    self.alpha_abs=alpha_abs					# absorption coefficient in cm^{-1}
-    self.prob_dest=prob_dest					# probability of destruction by collisional de-excitation
-    self.vth = np.sqrt( 2.0 * fc.kboltz * temp / fc.amu )       # thermal velocity in cm s^{-1}
-    self.Delta = la.nu0*self.vth/fc.clight                      # doppler width in Hz
-    self.a = la.Gamma/(4.0*np.pi*self.Delta)                    # damping parameter
-    self.sigma0 = la.osc_strength * fc.line_strength/(np.sqrt(np.pi)*self.Delta)  # line center cross section in cm^2
-    self.numden = tau0/(self.sigma0*radius)                     # H(1s) number density in cm^{-3}
-    self.k=self.numden*fc.line_strength*la.osc_strength         # in nu units. tau0=k*radius/(sqrt(pi)*delta)
-    self.xmax=np.rint(4.0*(self.a*tau0)**0.333)                 # wing 1.e4 times smaller than center
-    self.c1=np.sqrt(2.0/3.0)*np.pi/(3.0*self.a)               	# sigma(x) = c1*x^3, c1 = 0.855/a
-    self.c2 = self.c1**(2.0/3.0)*self.a/(np.pi*self.Delta)      # phi(sigma) = c2 / sigma**(2.0/3.0), c2=0.287*a**(1.0/3.0)/Delta
-    self.sigmas=self.c1*xsource**3
-    self.nsigma=nsigma
-    self.nmax=nmax
-    self.mmax=mmax
-    self.sigma_offset = 1e3
-    self.sigma_master = make_sigma_grids(self, xuniform=True)
-    self.sigma = np.array(sorted(np.concatenate(list(self.sigma_master.values()))))
+    '''
+    Stores physical parameters of the problem for a particular solve.
 
-def get_sigma_bounds(n, s, p):
+    Parameters
+    ----------
+    temp : float
+        Temperature of the gas in K.
+    tau0 : float
+        Line center optical depth of the sphere.
+    radius : float
+        Radius of the sphere in cm.
+    energy : float
+        Impulse energy in erg (set to 1 for normalization)
+    xsource : float
+        Frequency of source photons, expressed as the difference between the
+        source frequency and the line center in Doppler widths.
+    alpha_abs : float
+        Absorption coefficient in cm^{-1}.
+    prob_dest : float
+        Probability of destruction by collisional de-excitation.
+    nsigma : float
+        Number of frequency points to calculate solutions. Frequency resolution
+        of the solution.
+    nmax : float
+        Maximum number of spatial eigenmodes to compute.
+    mmax : float
+        Maximum number of frequency eigenmodes to compute.
 
-    kappan=n*np.pi/p.radius
-    wavenum = kappan*p.Delta/p.k
-    phi_crit = wavenum**2 * fc.clight*p.k / ( 3.0*np.abs(s)*p.Delta**2 )
-    x_tp = np.sqrt( p.a/(np.pi*p.Delta*phi_crit) )
-    sigma_tp = p.c1*x_tp**3
-    sigma_efold = p.k/(kappan*p.Delta)
+    Returns
+    ----------
+    `Parameters` object
+        An object storing physical parameters as attributes.
+    '''
 
-    sigma_left = -(sigma_tp + 23*sigma_efold) # TODO: Parametrize?
-    sigma_right = (sigma_tp + 23*sigma_efold)
-    source = p.sigmas
-    offset = p.sigma_offset
+    def __init__(self, temp, tau0, radius, energy, xsource,
+                 alpha_abs, prob_dest, nsigma, nmax, mmax):
+        self.temp = temp
+        self.tau0 = tau0
+        self.radius = radius
+        self.energy = energy
+        self.xsource = xsource
+        self.alpha_abs = alpha_abs
+        self.prob_dest = prob_dest
 
-    return ((sigma_left, min(source-offset, 0)), 
-            (min(source-offset, 0), max(source+offset, 0)),
-            (max(source+offset, 0), sigma_right))
-
-def make_sigma_grids(p, xuniform=True): ## Make master sigma grid uniform in x
-    
-    width = p.c1 * 50 * p.a * p.tau0
-    print("SIGMA GRID WIDTH: {:.1f}   ({:.0f} * tau0)    (x={:.1f})".format(width, width/p.tau0, np.cbrt(width/p.c1)))
-    source = p.sigmas
-    offset = p.sigma_offset
-    left, middle, right = ((-width, min(source-offset, 0)), 
-                           (min(source-offset, 0), max(source+offset, 0)),
-                           (max(source+offset, 0), width))
-
-    if xuniform:
-        left = np.cbrt(np.array(left)/p.c1)
-        right = np.cbrt(np.array(right)/p.c1)
-        middle = np.cbrt(np.array(middle)/p.c1)
-        source = p.xsource
-
-    # Determine the integration ranges
-    delimiters = sorted(np.array([left[0], source, 0., right[1]]))
-
-    # Build an array that's evenly spaced between leftmost and rightmost sigma
-    # The -1e-6 is to ensure the last data point, which is equal to sigma_right,
-    # does not end up in a bin all by itself
-    even_array = np.linspace(left[0], right[1]-1e-6, p.nsigma)
-
-    # Bin this evenly-spaced array into the integration ranges we found earlier
-    # These are the indices that specify which bin the data points fall into:
-    inds = np.digitize(even_array, delimiters)
-
-    # Count how many points have fallen into each integration range
-    nleft, nmiddle, nright = [len(inds[inds==i]) for i in range(1, 4)]
-
-    # For a nonzero source, make sure that the middle grid has enough points
-    if nmiddle < 4 and source != 0.:
-        warnings.warn('Middle grid is critically undersampled. Adding 4 points.')
-        nmiddle += 4
-        nright -= 2
-        nleft -= 2
-
-    ### Create grids
-    leftgrid = np.linspace(*left, nleft)
-    middlegrid = np.linspace(*middle, nmiddle)
-    rightgrid = np.linspace(*right, nright)
-
-    if xuniform:
-        leftgrid = p.c1 * leftgrid**3.
-        rightgrid = p.c1 * rightgrid**3.
-        middlegrid = p.c1 * middlegrid**3.
-
-    return {'left':leftgrid, 'middle':middlegrid, 'right':rightgrid}
+        self.vth = np.sqrt(2.0 * fc.kboltz * temp / fc.amu)                    # thermal velocity in cm s^{-1}
+        self.Delta = la.nu0 * self.vth / fc.clight                             # doppler width in Hz
+        self.a = la.Gamma / (4.0 * np.pi * self.Delta)                         # damping parameter
+        self.sigma0 = la.osc_strength * fc.line_strength / \
+            (np.sqrt(np.pi) * self.Delta)                                      # line center cross section in cm^2
+        self.numden = tau0 / (self.sigma0 * radius)                            # H(1s) number density in cm^{-3}
+        self.k = self.numden * fc.line_strength * la.osc_strength              # in nu units. tau0=k*radius/(sqrt(pi)*delta)
+        self.xmax = np.rint(4.0 * (self.a * tau0)**0.333)                      # wing 1.e4 times smaller than center
+        self.c1 = np.sqrt(2.0 / 3.0) * np.pi / (3.0 * self.a)                  # sigma(x) = c1*x^3, c1 = 0.855/a
+        self.c2 = self.c1**(2.0 / 3.0) * self.a / (np.pi * self.Delta)         # phi(sigma) = c2 / sigma**(2.0/3.0), c2=0.287*a**(1.0/3.0)/Delta
+        self.sigmas = self.c1 * xsource**3
+        self.nsigma = nsigma
+        self.nmax = nmax
+        self.mmax = mmax
+        self.sigma_offset = 1e3
+        self.sigma_master = make_sigma_grids(self, xuniform=True)
+        self.sigma = np.array(sorted(np.concatenate(list(self.sigma_master.values()))))
 
 
 if __name__ == '__main__':
-    energy=1.e0
-    temp=1.e4
-    tau0=1.e7
-    radius=1.e11
-    alpha_abs=0.0
-    prob_dest=0.0
-    xsource=0.0
-    nmax=6
-    nsigma=512
-    nomega=10
-    s = np.arange(0.2,-15.0,-0.01)
-    p = Parameters(temp,tau0,radius,energy,xsource,alpha_abs,prob_dest,nsigma,nmax)
-
+    pass
