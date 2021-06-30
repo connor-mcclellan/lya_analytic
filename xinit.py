adbc197c0951bf14520c9381b9f34e0d4a7ddeb9
mccbc <cmcclellan1010@gmail.com>
2021-06-30 12:21:28 -0400

    adding multiple tau0 to xinit.py, scaling axes by tauscale

--- xinit.py
+++ xinit.py
@@ -8,18 +8,28 @@ import scipy.integrate
 delta = 105691974558.58401
 a = 0.0004717036097750442
 
-fig, ax = plt.subplots(1, 1, figsize=(7, 5))
+def sigma(x, a):
+    return np.sqrt(2.0/3.0)*np.pi*x**3/(3.0*a)
 
-### PLOT THEORY LINES
-for i, x in enumerate([0., 2., 4., 6., 8., 10., 12.]):
-    tau0, xinit, temp, radius, L = (1e7, x, 1e4, 1e11, 1.)
-    x_ft, sigma_ft, Jp_ft, Hp_ft, Jsp_ft, Hsp_ft, Jh_ft, Hh_ft = ftsoln.ftsoln_wrapper(tau0, xinit, temp, radius, L)
-    norm = 4.0 * np.pi * radius**2 * delta * 4.0 * np.pi / L
-    yscale = xinit/tau0
-    ax.plot(x_ft, norm*Hh_ft/yscale, alpha=0.6, label=r'$x_s=${}'.format(xinit))
-    
-    
-plt.ylabel('P(x)')
-plt.xlabel('x')
-plt.legend()
-plt.show()
+def get_x_from_sigma(sigma, a):
+    return np.cbrt(sigma*(3.0*a)*np.sqrt(3.0/2.0)/np.pi)
+
+if __name__ == "__main__":
+
+    fig, ax = plt.subplots(1, 1, figsize=(7, 5))
+
+    ### PLOT THEORY LINES
+    for j, tau0 in enumerate([1e6, 1e7]):
+        for i, sigmas in enumerate([tau0, 2*tau0, 3*tau0, 4*tau0]):
+            x = get_x_from_sigma(sigmas, a)
+            tau0, xinit, temp, radius, L = (tau0, x, 1e4, 1e11, 1.)
+            x_ft, sigma_ft, Jp_ft, Hp_ft, Jsp_ft, Hsp_ft, Jh_ft, Hh_ft = ftsoln.ftsoln_wrapper(tau0, xinit, temp, radius, L)
+            norm = 4.0 * np.pi * radius**2 * delta * 4.0 * np.pi / L
+            tauscale = (a*tau0)**(1/3.)
+            ls = '-' if tau0==1e6 else '--'
+            ax.plot(x_ft/tauscale, norm*Hh_ft*tauscale**2, alpha=0.6, label=r'$x_s=${:.2f}'.format(xinit), ls=ls)
+            
+    plt.ylabel('P(x)')
+    plt.xlabel('x')
+    plt.legend()
+    plt.show()
