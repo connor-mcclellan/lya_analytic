f683c37a8a09af7a7526aa6cf556ef147e54e7c3
mccbc <cmcclellan1010@gmail.com>
2021-07-13 16:22:30 -0400

    finalizing fitting formula plot

--- eigencode/fitting_formula.py
+++ eigencode/fitting_formula.py
@@ -5,20 +5,26 @@ from mc_visual import mc_wait_time
 from scipy.special import gamma
 import matplotlib.pyplot as plt
 import matplotlib
+from matplotlib.patches import Patch
+from matplotlib.lines import Line2D
 matplotlib.rcParams['text.usetex'] = True
+matplotlib.rc('font', **{'family': 'serif',
+                         'serif': ['Computer Modern Roman']})
 import numpy as np
 import pdb
 
 fc=fundconst()
 la=lymanalpha()
 
-def monte_carlo(tau0, xinit):
+colorlegend = []
+
+def monte_carlo(tau0, xinit, c):
     print('Monte Carlo...')
     mc_dir = '/home/connor/Documents/lya_analytic/data/1M tau0_{:.1f}_xinit_{:.1f}_temp_10000.0_probabs_0.0/'.format(tau0, xinit)
     (bincenters, n), _, _ = mc_wait_time(mc_dir)
-    plt.scatter(bincenters, n, marker='+', label=r'MC $\tau_0={}'.format(scinot(tau0).split(r'\times ')[1]), alpha=0.7)
+    plt.scatter(bincenters, n, marker='s', s=2, label=r'MC $\tau_0={}'.format(scinot(tau0).split(r'\times ')[1]), alpha=0.7, color=c)
 
-def efunc(path, nmax, mmax):
+def efunc(path, nmax, mmax, c):
     print('Loading eigenfunctions...')
     directory = Path(path).resolve()
     Jsoln, ssoln, intJsoln, p = construct_sol(directory, nmax, mmax)
@@ -30,28 +36,32 @@ def efunc(path, nmax, mmax):
     print('Calculating wait time...')
     t = tlc * np.arange(0.1,140.0,0.1)
     P = waittime(Jsoln, ssoln, intJsoln, t, p)
-    plt.plot(t/tlc, P*tlc, label=r'Eigenfunction expansion, $\tau_0={}'.format(scinot(tau0).split(r'\times ')[1]))
+    plt.plot(t/tlc, P*tlc, alpha=0.7, label=r'Eigenfunction expansion, $\tau_0={}'.format(scinot(tau0).split(r'\times ')[1]), c=c)
 
     # Fitting function
     expfalloff = -ssoln[0, 0] * Pnmsoln[0, 0] * np.exp(ssoln[0, 0] * t)
     tdiff = tlc * np.cbrt(p.a * p.tau0)
-    plt.plot(t/tlc, np.exp(-(t/tdiff)**2.)*expfalloff * tlc, '--', label=r'Fitting function, $\tau_0={}'.format(scinot(tau0).split(r'\times ')[1]))
+    plt.plot(t/tlc, np.exp(-(tdiff/t/tlc)**2) * expfalloff * tlc, '--', lw=2, alpha=0.7, label=r'Fitting function, $\tau_0={}'.format(scinot(tau0).split(r'\times ')[1]), c=c)
+    colorlegend.append(Patch(facecolor=c, label=r'$\tau_0={}'.format(scinot(tau0).split(r'\times ')[1])))
 
 
+monte_carlo(1e6, 0.0, '#696969')
+monte_carlo(1e7, 0.0, 'k')
+efunc('./data/tau1e6_xinit0', 20, 500, '#696969')
+efunc('./data/210521_m500', 20, 500, 'k')
 
-monte_carlo(1e6, 0.0)
-monte_carlo(1e7, 0.0)
-efunc('./data/tau1e6_xinit0', 20, 500)
-efunc('./data/210521_m500', 20, 500)
-
+formatlegend = [Line2D([1], [0], color='k', label='Eigenfunctions'), Line2D([1], [1], ls='--', color='k', label='Fitting function'), Line2D([1], [0], color='k', ls='None', marker='s', ms=2, label='Monte Carlo')]
+fmtlegend = plt.legend(handles=formatlegend, loc='upper left', bbox_to_anchor=(0.7, 0.8), fontsize='x-small', frameon=False)
+clegend = plt.legend(handles=colorlegend, loc='lower left', bbox_to_anchor=(0.7, 0.8), fontsize='small', frameon=False)
+plt.gca().add_artist(clegend)
+plt.gca().add_artist(fmtlegend)
 
 # Plotting
 #plt.title('$x_s=0$')
-plt.title(r'$\tau_0=10^6$')
+#plt.title(r'$\tau_0=10^6$')
 plt.yscale('log')
-#plt.xscale('log')
+plt.xscale('log')
 plt.xlabel(r'$ct/R$')
 plt.ylabel('$(R/c)\, P(t)$')
 plt.ylim(1e-6, 5e-1)
-plt.legend()
 plt.show()
