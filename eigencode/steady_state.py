d52f491e536620248283dc9228f6ba86c3b8ff09
mccbc <cmcclellan1010@gmail.com>
2021-08-25 16:41:11 -0400

    cleaning up steady state code

--- eigencode/steady_state.py
+++ eigencode/steady_state.py
@@ -15,111 +15,47 @@ import numpy as np
 import pdb
 
 
-def generate_xuniform(sigma, p):
-    sigma_to_x = np.cbrt(sigma/p.c1)
-    xuniform_l = np.linspace(np.min(sigma_to_x), -0.1, int(len(sigma_to_x)/2))
-    xuniform_r = np.linspace(0.1, np.max(sigma_to_x), int(len(sigma_to_x)/2))
-    xuniform = np.concatenate([xuniform_l, xuniform_r])
-    return xuniform, sigma_to_x
-
-
-def fluence(sigma, p, Jsoln=None, ssoln=None, dijkstra=False):
-    '''
-    Calculates the fluence or luminosity by spatial eigenmode.
-    '''
-    #xuniform, sigma_to_x = generate_xuniform(sigma, p)
-    #sigma_xuniform = xuniform**3 * p.c1
-    #phi_xuniform = line_profile(sigma_xuniform, p)
-
+def steady_state_partial_sum(sigma, p):
     spec = np.zeros((p.nmax, np.shape(sigma)[0]))
-    #spec_xuniform = np.zeros((p.nmax, np.shape(xuniform)[0]))
     phi = line_profile(sigma, p)
-
-#    norm = (4.0 * np.pi * p.radius**2 * 4.0 * np.pi / p.energy)
-
-    if Jsoln is None and dijkstra is False:
-        # STEADY STATE SOLUTION
-        for n in range(1, p.nmax+1):
-
-            spec[n-1] = (
-                         - np.sqrt(6) * np.pi / 3. / p.k / phi
-                        * p.energy / p.radius * n * (-1)**n 
-                        * np.exp(-n * np.pi * p.Delta / p.k / p.radius * np.abs(sigma - p.sigmas))
-                        )
-
-    elif Jsoln is None and dijkstra is True:
-        H0 = (
-             np.sqrt(6) * p.energy / (3. * p.k * phi) / 32 / np.pi
-             / p.radius**3 / (np.cosh(np.pi * p.Delta / p.k / p.radius * (sigma - p.sigmas)) + 1)
-             )
-        F = 4 * np.pi * H0
-        spec[0] = 4 * np.pi * p.radius**2 * F
-
-    else:
-        # TIME DEPENDENT SOLUTION
-        for n in range(1, p.nmax+1):
-            for m in range(1, p.mmax+1):
-                spec[n-1] += (
-                             16. * np.pi**2 * p.radius * p.Delta
-                             / (3.0 * p.k * p.energy * phi) * (-1)**n
-                             * Jsoln[n-1, m-1, :] / ssoln[n-1, m-1]
-                             )
-
+    for n in range(1, p.nmax+1):
+        spec[n-1] = (
+                     - np.sqrt(6) * np.pi / 3. / p.k / phi
+                    * p.energy / p.radius * n * (-1)**n 
+                    * np.exp(-n * np.pi * p.Delta / p.k / p.radius * np.abs(sigma - p.sigmas))
+                    )
     return np.cbrt(sigma/p.c1), spec
 
 
-def mfluence(sigma, p, Jsoln=None, ssoln=None, dijkstra=False):
-    '''
-    Calculates the fluence or luminosity by frequency eigenmode.
-    '''
-    #xuniform, sigma_to_x = generate_xuniform(sigma, p)
-    #sigma_xuniform = xuniform**3 * p.c1
-    #phi_xuniform = line_profile(sigma_xuniform, p)
-
-    spec = np.zeros((p.mmax, np.shape(sigma)[0]))
-    #spec_xuniform = np.zeros((p.nmax, np.shape(xuniform)[0]))
+def dijkstra(sigma, p)
+    spec = np.zeros((p.nmax, np.shape(sigma)[0]))
     phi = line_profile(sigma, p)
-    norm = 1/(4.0 * np.pi * p.radius**2 * p.Delta * 4.0 * np.pi / p.energy)
-
-    if Jsoln is None and dijkstra is False:
-        # STEADY STATE SOLUTION
-        for n in range(1, p.nmax+1):
-            spec[0] += (                   ## FACTOR OF 2???
-                        norm * np.sqrt(6) * np.pi / 3. / p.k / p.Delta / phi
-                        * p.energy / p.radius * n * (-1)**n 
-                        * np.exp(-n * np.pi * p.Delta / p.k / p.radius * np.abs(sigma))
-                        )
-
-    elif Jsoln is None and dijkstra is True:
-        H0 = (
-             norm * np.sqrt(6) * p.energy / (3. * p.k * phi) / 32 / np.pi / p.Delta
-             / p.radius**3 / (np.cosh(np.pi * p.Delta / p.k / p.radius * sigma) + 1)
-             )
-        F = 4 * np.pi * H0
-        spec[0] = 4 * np.pi * p.radius**2 * F
-
-    else:
-        # TIME DEPENDENT SOLUTION
-        for n in range(1, p.nmax+1):
-          for m in range(1, p.mmax+1):
-              spec[m-1] += (
-                           norm * 16. * np.pi**2 * p.radius
-                           / (3.0 * p.k * p.energy * phi) * (-1)**n
-                           * Jsoln[n-1, m-1, :] / ssoln[n-1, m-1]
-                           )
-
+    H0 = (
+         np.sqrt(6) * p.energy / (3. * p.k * phi) / 32 / np.pi
+         / p.radius**3 / (np.cosh(np.pi * p.Delta / p.k / p.radius * (sigma - p.sigmas)) + 1)
+         )
+    F = 4 * np.pi * H0
+    spec[0] = 4 * np.pi * p.radius**2 * F
     return np.cbrt(sigma/p.c1), spec
 
+
+def time_integrated(sigma, p, Jsoln, ssoln)
+    spec = np.zeros((p.nmax, np.shape(sigma)[0]))
+    phi = line_profile(sigma, p)
+    for n in range(1, p.nmax+1):
+        for m in range(1, p.mmax+1):
+            spec[n-1] += (
+                         16. * np.pi**2 * p.radius * p.Delta
+                         / (3.0 * p.k * p.energy * phi) * (-1)**n
+                         * Jsoln[n-1, m-1, :] / ssoln[n-1, m-1]
+                         )
+    return np.cbrt(sigma/p.c1), spec
 
 
 if __name__ == '__main__':
     directory = Path('./data/tau1e7_xinit12').resolve()
     Jsoln, ssoln, intJsoln, p = construct_sol(directory, nmax=20, mmax=500)
 
-#    directory = Path('./data/210507_all').resolve()
-#    Jsoln2, ssoln2, intJsoln2, p2 = construct_sol(directory, nmax=20, mmax=100)
-
-#    x_t2, tdep_spec2 = fluence(p2.sigma, p2, Jsoln=Jsoln2, ssoln=ssoln2)
     x_t, tdep_spec = fluence(p.sigma, p, Jsoln=Jsoln, ssoln=ssoln)
     x_s, steady_state = fluence(p.sigma, p)
     x_d, dijkstra = fluence(p.sigma, p, dijkstra=True)
@@ -130,62 +66,10 @@ if __name__ == '__main__':
         ax.plot(x_t, np.sum(tdep_spec[:n], axis=0), '-', c='gray', marker='s', ms=2, lw=1, alpha=0.5, label='Time-integrated')
         ax.plot(x_d, dijkstra[0], '-', c='purple', alpha=0.7, lw=3, label=r'Steady State'.format(n))
         ax.plot(x_s, np.sum(steady_state[:n], axis=0), '--', c='c', lw=3, label='Steady State (Partial Sum)')
-#        ax.plot(x_t2, np.abs(np.sum(tdep_spec2[:n], axis=0)), 'm--', marker='^', ms=1, alpha=0.7, label=r'$n < 20$, $m < 100$'.format(n))
-        #plt.yscale('log')
         plt.ylim(-0.001, 0.05)
         plt.xlim(8, 30)
-#        plt.title('abs val of sum to n={}'.format(n))
         plt.xlabel('x')
         plt.ylabel('P(x)')
         plt.legend(loc=1)
         plt.tight_layout()
         plt.show()
-        #plt.savefig('timedep_v_steadystate_n{:03d}.pdf'.format(n))
-        plt.close()
-    '''
-
-    for m in range(1, p.mmax):
-        fig, ax = plt.subplots(1, 1)
-        ax.plot(x_s, np.abs(np.sum(steady_state[:5], axis=0)), '-', marker='s', ms=1, alpha=0.7, label='steady state')
-        ax.plot(x_d, np.abs(dijkstra[0]), '-', marker='s', ms=1, alpha=0.7, label=r'dijkstra')
-        ax.plot(x_t, np.abs(np.sum(tdep_spec[:m], axis=0)), 'r-', marker='o', ms=1, alpha=0.7, label=r'LSODA, $n < 5$, $m < 100$')
-        ax.plot(x_t2, np.abs(np.sum(tdep_spec2[:m], axis=0)), 'm--', marker='^', ms=1, alpha=0.7, label=r'RK45, $n < 5$, $m < 100$')
-        plt.yscale('log')
-        plt.ylim(1e-16, 1e-10)
-        plt.xlim(0, 30)
-        plt.title('n={}, m={}'.format(10, m))
-        plt.xlabel('x')
-        plt.legend()
-        plt.tight_layout()
-        plt.show()
-        #plt.savefig('timedep_v_steadystate_m{:03d}.pdf'.format(m))
-        plt.close()
-
-
-
-    
-    import matplotlib.pylab as pl
-    fig, ax = plt.subplots(1, 1)
-    colors = pl.cm.jet(np.linspace(0,1,p.nmax))
-
-    for n in range(10, p.nmax, 25):
-        #y = np.sum(steady_state[:n], axis=0)
-        y = np.abs(steady_state[n-1])
-        if n%2==0:
-            ls='--'
-        else:
-            ls='-'
-        ax.plot(x_s, y, ls, lw=1, marker='o', ms=1, c=colors[n-1], alpha=0.5)
-    sm = plt.cm.ScalarMappable(cmap=pl.cm.jet, norm=plt.Normalize(vmin=1, vmax=p.nmax))
-    cbar=plt.colorbar(sm)
-    cbar.ax.set_ylabel('n')
-    plt.yscale('log')
-#    plt.xscale('log')
-    plt.xlabel('$x$')
-    plt.tight_layout()
-    plt.show()
-    ''' 
-
-
-
-    
